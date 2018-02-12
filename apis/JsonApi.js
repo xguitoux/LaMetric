78e3c6bb430c6458d4bfa23a194696b9e25e23b8
Marsan Guillaume <gmarsan@sqli.com>
2018-02-12 11:18:36 +0100

    Cleaner promise method

--- apis/JsonApi.js
+++ apis/JsonApi.js
@@ -9,38 +9,7 @@ module.exports = {
      * @return json
      * returns Response called API method
      */
-    getData: function(url, headers, queryParams) {
-        return new Promise(function(resolve, reject) {
-
-            var postHeader = {
-                'User-Agent': headers.host,
-                'Accept': 'application/json',
-                'Authorization': headers.authorization // pass authorization header received in request
-            };
-            request.get({
-                url: url,
-                qs: queryParams,
-                headers: postHeader,
-                rejectUnauthorized: true,
-            }, function(error, response, body) {
-                var jsonBody = JSON.parse(body);
-
-
-                if (util.isNullOrUndefined(jsonBody)) {
-                    reject(new Error("JsonApi.getData - null response"));
-                } else
-                if (!error && response.statusCode == 200 && !jsonBody.error) {
-
-                    resolve(jsonBody);
-                } else {
-                    // console.log("** Error getData - " + jsonBody.error);
-                    // console.log("** url : " + url);
-                    reject(new Error(jsonBody.error));
-                }
-            });
-        });
-    },
-    getDataProm: function(url, p_headers, queryParams) {
+    getData: function(url, p_headers, queryParams) {
 
         var options = {
             uri: url,
@@ -51,29 +20,12 @@ module.exports = {
             },
             json: true // Automatically stringifies the body to JSON
         };
-        // console.log(options);
         return rp(options).promise().then(function(jsonBody) {
-
-            console.log("getDataProm dans THEN");
-            // console.log(jsonBody);
             return jsonBody;
-            // if (util.isNullOrUndefined(jsonBody)) {
-            //     reject(new Error("JsonApi.getData - null response"));
-            // } else
-            // if (!error && response.statusCode == 200 && !jsonBody.error) {
 
-            //process(jsonBody);
-            // } else {
-            //     reject(new Error(jsonBody.error));
-            // }
         }).catch(function(error) {
-            console.log("getDataProm dans CATCH");
+            console.log("JsonApi.getData CATCH");
             console.log(error);
-            // if (util.isNullOrUndefined(jsonBody)) {
-            //     reject(new Error("JsonApi.getData - null response"));
-            // } else {
-            //     reject(new Error(jsonBody.error));
-            // }
         });
     },
 
