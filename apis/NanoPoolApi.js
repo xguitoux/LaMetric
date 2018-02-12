f58f706e54678b894ee942d9962bd1833b379985
Marsan Guillaume <gmarsan@sqli.com>
2018-02-12 16:37:26 +0100

    Refacto & cleaning

--- apis/NanoPoolApi.js
+++ apis/NanoPoolApi.js
@@ -1,42 +1,60 @@
-var request = require('request');
-var Promise = require("bluebird");
-var jsonApi = require("./JsonApi");
+var request = require('request'),
+    Promise = require("bluebird"),
+    jsonApi = require("./JsonApi"),
+    jsonValues = require('../consts/jsonValues');
 
 
 module.exports = {
 
-    getMiningStats: function(headers, parameters) {
+    getMiningStatsOLD: function(headers, parameters) {
         var url = "https://api.nanopool.org/v1/eth/user/" + parameters.wallet;
         var self = this;
 
-        var dataProm = jsonApi.getData(url, headers, parameters).catch(function(err) {
-            console.log("## Error getMiningStats - " + err);
-            // console.log(typeof(err));
-            // console.log(err.toString());
-            console.log();
-            if (!err.toString().includes("Account not found")) {
-
-                return Promise.delay(5000).then(function() {
-                    self.getMiningStats(headers, parameters);
-                });
+        var dataProm = jsonApi.getData(url, headers, parameters).then(function(jsonBody) {
+            console.log("_________________");
+            console.log(jsonBody);
+            if (jsonBody.status != null) {
+                if (!jsonBody.status) {
+                    if (!jsonBody.error.toString().includes("Account not found")) {
+                        console.log("+++++++++++++");
+                        return Promise.delay(5000).then(function() {
+                            self.getMiningStats(headers, parameters);
+                        });
+                    } else {
+                        console.log("NanoPoolApi UNKNOWN ACCOUNT --> " + parameters.wallet);
+                        return jsonValues.unknowNanoAccount;
+                    }
+                } else {
+                    return jsonBody;
+                }
             }
-            //
         });
-        console.log();
-
-        // dataProm.then(function(dataaaaa) {
-        //     console.log("dataaaaa--->");
-        //     console.log(dataaaaa);
-        //     return Promise.resolve(dataProm);
-        // }).catch(function(errr) {
-        //     console.log("*********dataProm error=" + errr);
-        // });
         return dataProm;
     },
-    // getCoinChange: function(headers, parameters) {
-    //     var url = " https://min-api.cryptocompare.com/data/price?fsym=" + parameters.coin + "&tsyms=" + parameters.currency;
+    getMiningStats: function(headers, parameters) {
+        var self = this;
+        return new Promise(function(resolve) {
+            var url = "https://api.nanopool.org/v1/eth/user/" + parameters.wallet;
 
-    //     return jsonApi.getData(url, headers, parameters);
-
-    // },
+            return jsonApi.getData(url, headers, parameters).then(function(jsonBody) {
+                // console.log("______DANS THEN___________");
+                // console.log(jsonBody);
+                if (jsonBody.status != null) {
+                    if (!jsonBody.status) {
+                        if (!jsonBody.error.toString().includes("Account not found")) {
+                            Promise.delay(2500).then(function() {
+                                return self.getMiningStats(headers, parameters);
+                            });
+                        } else {
+                            console.log("NanoPoolApi UNKNOWN ACCOUNT --> " + parameters.wallet);
+                            resolve(jsonValues.unknowNanoAccount);
+                        }
+                    } else {
+                        // console.log("On resolve avec la bonne value");
+                        resolve(jsonBody);
+                    }
+                }
+            });
+        });
+    },
 };
